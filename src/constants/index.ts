1e06187ec72e3068b89d742b8233a1c479da1bb7
AASHRITH <aashrith@Mac.lan>
2026-01-20 16:59:06 +0530

    Rest Data Provided

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -55,6 +55,18 @@ export const ALLOWED_TYPES = [
     "image/webp",
 ];
 
+const getEnvVar = (key: string): string => {
+    const value = import.meta.env[key];
+    if (!value) {
+        throw new Error(`Missing environment variable: ${key}`);
+    }
+    return value;
+};
+
+
+export const BACKEND_BASE_URL = getEnvVar("VITE_BACKEND_BASE_URL");
+
+export const BASE_URL =  import.meta.env.VITE_API_URL;
 
 export const teachers = [
     {
